e1307ed1f8267e8eace7a16e2301afcd06b6fbd6
张小花Soleil <94184457+Soleil-1008@users.noreply.github.com>
2021-11-18 14:00:48 +0800

    Add files via upload

---
+++ test_21-11-13/test_21-11-13_1.c
@@ -0,0 +1,29 @@
+//包含一个叫stdio.h的文件
+//std-标准 standard input output
+
+#include <stdio.h>
+int main() //主函数-程序的入口-main函数有且仅有一个
+{
+	//这里完成任务
+	//在屏幕上输出hello world
+	//函数-printf function-printf-打印函数
+	//库函数-c语言本身提供给我们使用的函数
+	//别人的东西-打招呼
+	//#include   #指令形式
+
+	printf("hello world\n");
+	printf("hehe\n");
+	//\n表示换行
+	return 0;
+	//返回 0
+}
+
+/*int是整型的意思
+main前的int表示main函数调用返回一个整型值
+int main()
+{
+	return 0;返回0
+}
+过时的写法
+void main(){}
+*/
