1c58def41891f2a6cb755490e879c68b8a2a8dc8
张小花Soleil <94184457+Soleil-1008@users.noreply.github.com>
2021-11-25 22:29:43 +0800

    Add files via upload

---
+++ test_21-11-23/test_21-11-23/test.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//static int g_val = 2021;//error test_21-11-23_1不能使用
+int g_val = 2021;//全局变量
+//定义一个函数
+/*
+static int Add(int x, int y) {
+	int z = x + y;
+	return z;
+}
+*/
+int Add(int x, int y) {
+	int z = x + y;
+	return z;
+}
