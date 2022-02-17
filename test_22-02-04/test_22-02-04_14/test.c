dfda8cb618711db00e11a8051561191b5e24bd25
张小花Soleil <94184457+Soleil-1008@users.noreply.github.com>
2022-02-17 16:10:50 +0800

    Add files via upload

---
+++ test_22-02-04/test_22-02-04_14/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//函数的声明和定义
+//分文件的书写形式
+#include<stdio.h>//库里面的头文件
+#include "add.h"//引自己写的头文件用双引号
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int sum = 0;
+	//函数调用
+	sum = Add(a, b);
+	printf("%d\n", sum);//30
+	return 0;
+}
