3153eb506b07144798a7ecd75884a6a0f96b3e4d
张小花Soleil <94184457+Soleil-1008@users.noreply.github.com>
2022-03-22 19:25:15 +0800

    Add files via upload

---
+++ test_22-03-17/test_22-03-17_10.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//数组名
+//数组名是数组首元素的地址。（有两个例外）
+//1.sizeof(数组名)--数组名表示整个数组，计算的是整个数组的大小，单位是字节
+//2.&s数组名--数组名表示整个数组,取出的是整个数组的地址
+#include<stdio.h>
+int main() {
+	int arr[10] = { 1,2,3,4,5,6,7};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	printf("%p\n", arr);//数组名-首元素地址000000960B1CF8F8
+	printf("%p\n", arr+1);//+4
+	printf("%p\n", &arr[0]);//取的是首元素地址000000960B1CF8F8
+	printf("%p\n", &arr[0]+1);//+4
+	printf("%d\n", *arr);//1 解引用
+	printf("%p\n", &arr);//取出的是数组的地址00000098B06FF988
+	printf("%p\n", &arr+1);//+28
+	return 0;
+}
